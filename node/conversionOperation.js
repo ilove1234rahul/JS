ce091ac3b899768ed94f65f038127cc9591e5131
Rahul Mondal <138432540+ilove1234rahul@users.noreply.github.com>
2024-07-11 15:07:24 +0000

    conversionOperation

---
+++ node/conversionOperation.js
@@ -0,0 +1,31 @@
+  //Convert into Number:
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0;
+
+
+//Convert into Bollean:
+  let isloggedIn = 1
+   
+  let booleanIsloggedIn = Boolean(isloggedIn)
+  console.log(booleanIsloggedIn);
+
+  // 1 => ture; 0 => false;
+  // " " => false
+  // " Rahul" => true
+
+  //Conert into String:
+  let someString = 33
+  let stringNumber = String(someString)
+  console.log( typeof stringNumber);
+    
+
