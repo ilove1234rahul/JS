4c2a001da1b97e00f8b00c4217302a3a876a32cf
Rahul Mondal <138432540+ilove1234rahul@users.noreply.github.com>
2024-07-11 14:38:46 +0000

    Variable

---
+++ node/Variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = " memondalrahul@gmail.com"
+var accountPassword = "12345"
+accountCity = " Asansol"
+ 
+// accountId = 2 (Not Allowed)
+accountEmail = "raj@gmail.com"
+accountPassword = "234"
+accountCity = "Kolkata"
+
+console.log(accountId);
+
+//All Variables ko Ak st Print kr skta ha:
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ node/datatyes.js
@@ -0,0 +1,27 @@
+ "use strict" ; //trat all JS Code As newer version
+
+ // alert(3+3) we are useing nodejs, not browser
+
+ console.log(3 
+
+  + 3 ) //code readability shoud be high
+    
+  //Data Types:
+   let name = "hitesh"
+   let age = 18
+   let isLoggedIn = false
+     
+   // This all Are Primetive Data types:
+   // number => 2 to power 53
+   // bigint ( Big Numbers Stored krna ka leya)
+   //string => ""
+   //boolean => ture/ false
+   // null => standanlone value
+   // undefine =>
+    //Symble => unique
+
+   //object
+
+   //Data types ka Types Checked krna ka:
+   console.log(typeof null);     // object
+   console.log(typeof undefined);  // undefine
